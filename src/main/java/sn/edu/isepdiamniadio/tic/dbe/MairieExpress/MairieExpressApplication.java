e90c4fae7cab3a12b74c39a2469641877699f452
Diamiljoop <diamiboubou238@gmail.com>
2024-10-08 17:08:11 +0200

    initialisation du projet

---
+++ src/main/java/sn/edu/isepdiamniadio/tic/dbe/MairieExpress/MairieExpressApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.isepdiamniadio.tic.dbe.MairieExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MairieExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MairieExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/isepdiamniadio/tic/dbe/MairieExpress/MairieExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.isepdiamniadio.tic.dbe.MairieExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MairieExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
